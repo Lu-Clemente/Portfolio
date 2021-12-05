8ac73000f99c10840a8984711fd15af5b1e83163
Lu Clemente <luciene.clemente@unifei.edu.br>
2021-12-05 04:24:13 -0300

    Fixes

--- src/components/About.js
+++ src/components/About.js
@@ -41,7 +41,7 @@ const About = ()=>{
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </Description>
                     <blockquote className="quote">
-                        "I will show you how great I am."
+                        I will show you how great I am.
                         <br />
                         <span>Muhammad Ali</span>
                     </blockquote>
